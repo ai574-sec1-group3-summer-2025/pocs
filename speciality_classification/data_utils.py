cddd643608a2f7dc23dbec9dc4fd545e6be2192a
Mahfuzur Rahman <mahfuzur.rahman@imalogix.com>
2025-07-18 18:30:25 -0400

    run distilbiobert with additional data preprocessing

--- speciality_classification/data_utils.py
+++ speciality_classification/data_utils.py
@@ -12,7 +12,7 @@ class DataUtils:
 
     def clean_text(self, text):
         text = text.lower()
-        text = re.sub(r'[^a-z0-9\s]', ' ', text)
+        text = re.sub(r'[^a-z\s]', ' ', text)
         text = re.sub(r'\s+', ' ', text).strip()
         return text
     
